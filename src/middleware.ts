a549f283dd1675754ed73560625d1ad8bb65a1b6
mazinhoandrade <irismarandrade94@gmail.com>
2025-06-06 18:04:21 -0300

    feat: middleware pra validação do cookies

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { getSessionCookie } from "better-auth/cookies";
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = (request: NextRequest) => {
+  const sessionCookie = getSessionCookie(request);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/authentication", request.url));
+  }
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/appointments/:path*",
+    "/clinic-form/:path*",
+    "/doctors/:path*",
+    "/patients/:path*",
+    "/subscription/:path*",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
